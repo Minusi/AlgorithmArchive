3d2a4b9a938c784e8f1876f9e754fac3248b2804
Minusi <minusie357@gmail.com>
2019-10-28 13:03:27 +0900

    CodeTest _ SmileGate CodeTest(중앙값 큐 만들기, 로그 분석하기) 풀이 및 프로젝트명 "ConsoleApplication1"을 "CodeTest"로 변경

---
+++ CodeTest/우주 탐사하기.cpp
@@ -0,0 +1,75 @@
+﻿//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//
+///* 비트 비교 방식으로 틀린 갯수 반환 */
+//int Check_Internal(unsigned char Low, unsigned char High, unsigned char Mid)
+//{
+//	int Result = 0;
+//	unsigned char BitPos = 1;
+//	for (int i = 0; i < 8; ++i)
+//	{
+//		BitPos = 1 << i;
+//		
+//		int LFlag = Low &  BitPos;
+//		int HFlag = High &  BitPos;
+//		int MFlag = Mid &  BitPos;
+//		if ((LFlag != HFlag)
+//			|| (HFlag != MFlag)
+//			|| LFlag != MFlag)
+//			Result++;
+//	}
+//
+//	return Result;
+//}
+//
+///* 토큰화한 후, 비교값을 출력하는 함수 */
+//int Check(char* const Low, char* const High, char* const Mid,int N)
+//{
+//	int Result = 0;
+//
+//	/* - 토큰으로 문자열 구분하는 구간 */
+//	char* LToken, *HToken, *MToken;
+//	LToken = strtok(Low, "-");
+//	HToken = strtok(High, "-");
+//	MToken = strtok(Mid, "-");
+//	size_t End = strlen(LToken);
+//
+//	char* Endptr;
+//	while (N--)
+//	{
+//		unsigned char LValue = strtol(LToken, &Endptr, 10);
+//		unsigned char HValue = strtol(HToken, &Endptr, 10);
+//		unsigned char MValue = strtol(MToken, &Endptr, 10);
+//		Result += Check_Internal(LValue, HValue, MValue);
+//
+//		LToken = strtok(LToken + End + 1, "-");
+//		HToken = strtok(HToken + End + 1, "-");
+//		MToken = strtok(MToken + End + 1, "-");
+//	}
+//	return Result;
+//}
+//
+//int main()
+//{
+//	int T;
+//	scanf("%d", &T);
+//	
+//	int N;
+//	char Low[2000000];
+//	char High[2000000];
+//	char Mid[2000000];
+//	while (T--)
+//	{
+//		scanf("%d", &N);
+//		
+//		scanf("%s", Low);
+//		scanf("%s", High);
+//		scanf("%s", Mid);
+//		
+//		/* 출력 */
+//		printf("%d\n", Check(Low, High, Mid, N));
+//	}
+//
+//	return 0;
+//}
---
+++ CodeTest/중앙값 큐 만들기.cpp
@@ -0,0 +1,120 @@
+﻿//#include <stdio.h>
+//#include <queue>
+//#include <functional>
+//#include <cstring>
+//using namespace std;
+//
+///* [Notice for C/C++]
+//* - 기본 제공되는 뼈대 코드는 입출력의 이해를 돕기위해 제공되었습니다.
+//* - 뼈대코드의 활용은 선택사항이며 코드를 직접 작성하여도 무관합니다.
+//*
+//* - 데이터의 입력과 출력은 stdin과 stdout을 통하고 scanf와 printf를 사용하세요
+//* - 별도의 병렬 처리나 시스템콜, 네트워크/파일접근 등을 하지 마세요
+//* - main은 int형으로 선언한 후 return 0를 항상 하는 것을 권유합니다
+//* - 지역변수 배열 크기에 의한 Stack Overflow 에러에 주의하세요
+//* - (C++) cin/cout보다 scanf/printf를 사용하기를 권장합니다. (입출력 성능에 의한 이유)
+//*/
+//#pragma warning(disable:4996)
+//
+//
+///* 큐의 좌측에 해당되는 MaxQueue의 top값이 중앙값임을 알려주는 중앙값 큐입니다. */
+//class MedianQueue
+//{
+//public:
+//	/* 중앙값 큐에 Push합니다. 이후 중앙값을 출력합니다. */
+//	void Push(int X)
+//	{
+//		/* 현재 저장된 수가 짝수개일 경우, MinQueue에 값을 넣고 MinQueue의 top 값 1개를 MaxQueue로 시프트*/
+//		if (Size_Internal() % 2 == 0)
+//		{
+//			MinQueue.push(X);
+//			MaxQueue.push(MinQueue.top());
+//			MinQueue.pop();
+//		}
+//		/* 현재 저장된 수가 짝수개일 경우, MaxQueue에 값을 넣고 MaxQueue의 top 값 1개를 MinQueue로 시프트*/
+//		else
+//		{
+//			MaxQueue.push(X);
+//			MinQueue.push(MaxQueue.top());
+//			MaxQueue.pop();
+//		}
+//
+//		printf("%d\n", MaxQueue.top());
+//	}
+//
+//	/* 중앙값 큐의 Pop을 수행합니다. 중앙값이 빠집니다. 이후 중앙값이 있다면, 중앙값을
+//	 * 없다면, NO ITEM을 출력합니다. */
+//	void Pop()
+//	{
+//		if (Size_Internal() == 0)
+//		{
+//			printf("NO ITEM\n");
+//			return;
+//		}
+//
+//		MaxQueue.pop();
+//		/* pop 이후, 중앙값 큐의 개수가 홀수 개이면, 우측 MinQueue 값 하나를 MaxQueue로 시프트 */
+//		if (Size_Internal() % 2 == 1)
+//		{
+//			MaxQueue.push(MinQueue.top());
+//			MinQueue.pop();
+//		}
+//
+//		if (Size_Internal() == 0)
+//			printf("NO ITEM\n");
+//		else
+//			printf("%d\n", MaxQueue.top());
+//	}
+//
+//	/* 큐의 총 사이즈를 출력합니다. */
+//	void Size()
+//	{
+//		printf("%d\n", Size_Internal());
+//	}
+//
+//private:
+//	/* 큐 사이즈를 반환하는 Size 내부 함수 */
+//	int Size_Internal()
+//	{
+//		int QueueSize = MaxQueue.size() + MinQueue.size();
+//		return QueueSize;
+//	}
+//
+//private:
+//	/* 중앙값 + 1 ~최대값 저장용 큐 */
+//	priority_queue<int, vector<int>, greater<int>> MinQueue;
+//	/* 최솟값 ~ 중앙값 저장용 큐 */
+//	priority_queue<int, vector<int>, less<int>> MaxQueue;
+//};
+//
+//int main()
+//{   //프로그램의 시작부 
+//
+//	int N;
+//	scanf("%d", &N);
+//
+//	MedianQueue MidQueue;
+//
+//	char Buffer[100];
+//	int Value;
+//	while (N--)
+//	{
+//		/* 입력 인터프리터 */
+//		scanf("%s", Buffer);
+//		if (strcmp(Buffer, "PUSH") == 0)
+//		{
+//			scanf("%d", &Value);
+//			MidQueue.Push(Value);
+//		}
+//		else if (strcmp(Buffer, "POP") == 0)
+//		{
+//			MidQueue.Pop();
+//		}
+//		else if(strcmp(Buffer, "SIZE") == 0)
+//		{
+//			MidQueue.Size();
+//		}
+//	}
+//
+//	return 0;
+//}
