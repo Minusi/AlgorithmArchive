6e5fd4ef9377a345a517653948eb290f394395db
Minusi <minusie357@gmail.com>
2019-11-05 03:01:55 +0900

    Baekjoon _ (Category)  Implementation Algorithm(8979), Simulation Algorithm(14891) 풀이

---
+++ BackjoonAlgorithm/14891. 톱니바퀴.cpp
@@ -0,0 +1,112 @@
+﻿//#include <iostream>
+//#include <string>
+//#include <cmath>
+//#include <deque>
+//using namespace std;
+//
+////#define DEBUG
+//#ifdef DEBUG
+//#include <chrono>
+//#include "Utility.h"
+//
+//chrono::system_clock::time_point Start;
+//chrono::nanoseconds Time;
+//#endif
+//
+//
+//
+//void CheckGearPolarity(const deque<int>(&Gear)[5], int (&DesiredRot)[5], int SrcNum, int SrcRot, int DestNum = -1)
+//{
+//	if (DestNum == -1)
+//	{
+//		DesiredRot[SrcNum] = SrcRot;
+//		CheckGearPolarity(Gear, DesiredRot, SrcNum, SrcRot, SrcNum + 1);
+//		CheckGearPolarity(Gear, DesiredRot, SrcNum, SrcRot, SrcNum - 1);
+//		return;
+//	}
+//
+//	if (SrcNum < 1 || DestNum < 1 || SrcNum > 4 || DestNum > 4)
+//		return;
+//
+//	int SrcPolar, DestPolar;
+//	if (SrcNum < DestNum)
+//	{
+//		SrcPolar = Gear[SrcNum].at(2);
+//		DestPolar = Gear[DestNum].at(6);
+//	}
+//	else if (SrcNum > DestNum)
+//	{
+//		SrcPolar = Gear[SrcNum].at(6);
+//		DestPolar = Gear[DestNum].at(2);
+//	}
+//
+//	if (SrcPolar != DestPolar && SrcRot != 0)
+//		DesiredRot[DestNum] = -SrcRot;
+//	else
+//		DesiredRot[DestNum] = 0;
+//
+//	SrcNum > DestNum ?
+//		CheckGearPolarity(Gear, DesiredRot, DestNum, DesiredRot[DestNum], DestNum - 1) :
+//		CheckGearPolarity(Gear, DesiredRot, DestNum, DesiredRot[DestNum], DestNum + 1);
+//}
+//
+//void RotateGear(deque<int>(&Gear)[5], const int(&DesiredRot)[5])
+//{
+//	for (int i = 1; i <= 4; ++i)
+//	{
+//		/* 반시계 방향 */
+//		if (DesiredRot[i] == -1)
+//		{
+//			Gear[i].push_back(Gear[i].front());
+//			Gear[i].pop_front();
+//		}
+//		/* 회전 금지 */
+//		else if (DesiredRot[i] == 0)
+//		{
+//			continue;
+//		}
+//		/* 시계 방향 */
+//		else if(DesiredRot[i] == 1)
+//		{
+//			Gear[i].push_front(Gear[i].back());
+//			Gear[i].pop_back();
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+//
+//	deque<int> Gear[5];
+//	int DesiredRot[5];
+//	string Buffer;
+//	for (int i = 1; i <= 4; ++i)
+//	{
+//		cin >> Buffer;
+//		for (int j = 0; j < 8; ++j)
+//			Gear[i].push_back(Buffer[j] - '0');
+//	}
+//
+//	int K, TgtGear, RotateDir;
+//	cin >> K;
+//
+//	for (int i = 0; i < K; ++i)
+//	{
+//		cin >> TgtGear >> RotateDir;
+//		CheckGearPolarity(Gear, DesiredRot, TgtGear, RotateDir);
+//		RotateGear(Gear, DesiredRot);
+//	}
+//
+//	int Answer = 0;
+//	for (int i = 1; i <= 4; ++i)
+//	{
+//		
+//		if (Gear[i].front() == 1)
+//			Answer += pow(2, i - 1);
+//	}
+//
+//	cout << Answer << "\n";
+//	return 0;
+//}
---
+++ BackjoonAlgorithm/8979. 올림픽.cpp
@@ -0,0 +1,91 @@
+﻿//#include <iostream>
+//#include <tuple>
+//#include <algorithm>
+//#include <functional>
+//using namespace std;
+//
+////#define DEBUG
+//#ifdef DEBUG
+//#include <chrono>
+//#include "Utility.h"
+//
+//chrono::system_clock::time_point Start;
+//chrono::nanoseconds Time;
+//#endif
+//
+//
+//
+//int main()
+//{
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+//
+//	tuple<int, int, int, int> Nations[1001];
+//
+//	int N, K;
+//#ifdef DEBUG
+//	N = GenerateSingleTestData<int>(10, 15);
+//	K = GenerateSingleTestData<int>(1, 10);
+//	cout << N << " " << K << "\n";
+//#else
+//	cin >> N >> K;
+//#endif // DEBUG
+//
+//
+//	int Nation, Gold, Silver, Bronze;
+//	for (int i = 1; i <= N; ++i)
+//	{
+//#ifdef DEBUG
+//		Nation = i;
+//		Gold = GenerateSingleTestData<int>(0, 5);
+//		Silver = GenerateSingleTestData<int>(0, 5);
+//		Bronze = GenerateSingleTestData<int>(0, 5);
+//		cout << Nation << " " << Gold << " " << Silver << " " << Bronze << "\n";
+//#else
+//		cin >> Nation >> Gold >> Silver >> Bronze;
+//#endif // DEBUG
+//		get<0>(Nations[Nation]) = Gold;
+//		get<1>(Nations[Nation]) = Silver;
+//		get<2>(Nations[Nation]) = Bronze;
+//		get<3>(Nations[Nation]) = Nation;
+//	}
+//	sort(Nations + 1, Nations + N + 1, greater<tuple<int,int,int,int>>());
+//
+//#ifdef DEBUG
+//	cout << "\n";
+//	for (int i = 1; i <= N; ++i)
+//	{
+//		cout << get<3>(Nations[i]) << " " << get<0>(Nations[i]) << " " << get<1>(Nations[i]) << " "<<  get<2>(Nations[i]) << "\n";
+//	}
+//#endif // DEBUG
+//
+//	
+//	tuple<int, int, int, int> Prev(-1,-1,-1,-1);
+//	int Grade = 0;
+//	int AccumulateSum = 0;
+//	for (int i = 1; i <= N; ++i)
+//	{
+//		if (get<0>(Nations[i]) != get<0>(Prev) ||
+//			get<1>(Nations[i]) != get<1>(Prev) ||
+//			get<2>(Nations[i]) != get<2>(Prev))
+//		{
+//			Grade += 1 + AccumulateSum;
+//			AccumulateSum = 0;
+//		}
+//		else
+//		{
+//			AccumulateSum++;
+//		}
+//
+//		
+//		if (get<3>(Nations[i]) == K)
+//		{
+//			cout << Grade << "\n";
+//			break;
+//		}
+//
+//		Prev = Nations[i];
+//	}
+//
+//	return 0;
+//}
